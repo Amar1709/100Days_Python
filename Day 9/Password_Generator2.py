79faeb98ee778e0c53b7ae9866a2e61ae7ad7f1c
Amar <amar.parameswaran.btech2019@sitpune.edu.in>
2022-12-14 07:52:15 +0530

    Changes to password gen2

---
+++ Day 9/Password_Generator2.py
@@ -0,0 +1,12 @@
+# Password Generator using list comprehensions
+
+from unicodedata import digit
+
+
+alph = [chr(i) for i in range(97, 123)]
+alph2 = [chr(i) for i in range(65, 91)]
+
+digits = [chr(i) for i in range(48, 58)]
+
+spec = [chr(i) for i in range(33, 48)]
+
